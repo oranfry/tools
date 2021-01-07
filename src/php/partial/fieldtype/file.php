483b7e7e12317c83db5b929577bbc3b5de3b4cf1
Oran Fry <oran@emerg.co>
2021-01-07 20:14:37 +1300

    File Field
    Only softly hide elements, so they can be shown again if needed.

--- src/php/partial/fieldtype/file.php
+++ src/php/partial/fieldtype/file.php
@@ -1,24 +1,20 @@
 <span class="file-field-controls">
-    <?php if ($value || @$bulk): ?>
-        <span class="file-field-controls__actions">
-            <?php if (!@$bulk): ?>
-                <div style="margin-bottom: 0.5em"><a href="/api/download/<?= $value ?>" download><i class="icon icon--gray icon--<?= @$field->translate[$field->icon] ?? $field->icon ?>"></i></a></div>
-            <?php endif ?>
-            <span class="button file-field-controls__change"><?= @$bulk ? 'choose' : 'change' ?></span>
-            <span class="button file-field-controls__delete">delete</span>
-            <?php if (@$field->generable): ?>
-                <span class="button file-field-controls__generate">generate</span>
-            <?php endif ?>
-        </span>
-    <?php endif ?>
-    <span class="file-field-controls__input" <?= $value || @$bulk ? 'style="display:none"' : '' ?>>
-        <input class="field value" type="file" name="<?= $field->name ?>" style="width: 16em">
-        <?php if (@$field->generable && !$value && !@$bulk): ?>
+    <span class="file-field-controls__actions" <?= !$value && !@$bulk ? 'style="display:none"' : '' ?>>
+        <?php if (!@$bulk): ?>
+            <div style="margin-bottom: 0.5em"><a href="/api/download/<?= $value ?>" download><i class="icon icon--gray icon--<?= @$field->translate[$field->icon] ?? $field->icon ?>"></i></a></div>
+        <?php endif ?>
+        <span class="button file-field-controls__change"><?= @$bulk ? 'choose' : 'change' ?></span>
+        <span class="button file-field-controls__delete">delete</span>
+        <?php if (@$field->generable): ?>
             <span class="button file-field-controls__generate">generate</span>
         <?php endif ?>
-        <?php if ($value || @$bulk): ?>
-            <span class="button file-field-controls__cancel">cancel</span>
+    </span>
+    <span class="file-field-controls__input" <?= $value || @$bulk ? 'style="display:none"' : '' ?>>
+        <input class="field value" type="file" name="<?= $field->name ?>" style="width: 16em">
+        <?php if (@$field->generable): ?>
+            <span class="button file-field-controls__generate" <?= !$value && !@$bulk ? 'style="display: none"' : '' ?>>generate</span>
         <?php endif ?>
+        <span class="button file-field-controls__cancel" <?= !$value && !@$bulk ? 'style="display:none"' : '' ?>>cancel</span>
     </span>
     <span class="file-field-controls__willdelete" style="display:none">
         Will delete
