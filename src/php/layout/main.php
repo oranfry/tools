267e0650ffd9d589bc3de3ba71ca468f4db178c9
Oran Fry <oran@emerg.co>
2021-09-29 22:14:58 +1300

    Layout
    Add a new optional area above the flash messages for customisation by the view.

--- src/php/layout/main.php
+++ src/php/layout/main.php
@@ -11,6 +11,7 @@
     <?php require search_plugins('src/php/partial/tools/nav.php'); ?>
     <div class="wrapper">
         <?php if (BACK): ?><br><div class="only-super1200"><a class="sidebar-backlink" href="<?= BACK ?>">Back</a></div><?php endif ?>
+        <?php if ($preflash_view = search_plugins('src/php/partial/preflash/' . (defined('VIEW') ? VIEW : PAGE) . '.php')): ?><?php require $preflash_view; ?><?php endif ?>
         <div class="flash"><?php foreach (is_array(@$flash) ? $flash : [] as $message): ?>
             <div class="flash__message <?= @$message->type ? "flash__message--{$message->type}" : null ?>"><?= $message->message ?></div>
         <?php endforeach ?></div>
