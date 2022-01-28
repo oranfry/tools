caf98a8b4c515e870b178c9bf3b9d096caf3e48c
Oran Fry <oran@emerg.co>
2022-01-29 00:34:51 +1300

    PHP Syntax
    Corrected.

--- src/php/controller/tools/relay/blend/update.php
+++ src/php/controller/tools/relay/blend/update.php
@@ -1,6 +1,6 @@
 <?php
 
-$api = ApiClient::(AUTH_TOKEN, APIURL);
+$api = ApiClient::http(AUTH_TOKEN, APIURL);
 $data = json_decode(file_get_contents('php://input'));
 
 return ['data' => $api->bulkupdate(BLEND_NAME, $data, get_query_filters())];
