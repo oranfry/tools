bca6d46c5846195193ee38f0ef04d61b47bfa409
Oran Fry <oran@emerg.co>
2023-04-07 17:13:52 +1200

    Views
    Use the new way to move through the view hierarchy.

--- src/php/layout/main.php
+++ src/php/layout/main.php
@@ -7,17 +7,24 @@
     <title><?= @$title ?? PAGE ?></title>
 </head>
 <body class="wsidebar">
-    <?php require search_plugins('src/php/partial/tools/nav.php'); ?>
+    <?php ss_require('src/php/partial/tools/nav.php', $viewdata); ?>
     <div class="wrapper">
         <?php if (BACK): ?><br><div class="only-super1200"><a class="sidebar-backlink" href="<?= BACK ?>">Back</a></div><?php endif ?>
-        <?php if ($preflash_view = search_plugins('src/php/partial/preflash/' . (defined('VIEW') ? VIEW : PAGE) . '.php')): ?><?php require $preflash_view; ?><?php endif ?>
+        <?php ss_include('src/php/partial/preflash/' . (defined('VIEW') ? VIEW : PAGE) . '.php', $viewdata); ?>
         <div class="flash"><?php foreach (is_array(@$flash) ? $flash : [] as $message): ?>
             <div class="flash__message <?= @$message->type ? "flash__message--{$message->type}" : null ?>"><?= $message->message ?></div>
         <?php endforeach ?></div>
-        <?php require search_plugins('src/php/partial/content/' . (defined('VIEW') ? VIEW : PAGE) . '.php'); ?>
+        <?php ss_require('src/php/partial/content/' . (defined('VIEW') ? VIEW : PAGE) . '.php', $viewdata); ?>
     </div>
 
+    <form id="instanceform">
+        <div style="display: none;">
+            <?php foreach (ContextVariableSet::getAll() as $active) : ?><?php $active->inputs(); ?><?php endforeach ?>
+            <div id="new-vars-here"></div>
+        </div>
+    </form>
+
     <script type="text/javascript" src="/build/js/app.<?= latest('js') ?>.js"></script>
-    <?php @include APP_HOME . '/src/php/partial/js/' . PAGE . '.php'; ?>
+    <?php ss_include('src/php/partial/js/' . PAGE . '.php', $viewdata); ?>
 </body>
 </html>
