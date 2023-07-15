d515f134c154ac39e4cd61c79598bdaa6bb7741a
Oran Fry <oran@emerg.co>
2023-07-15 12:07:26 +1200

    Fields Types
    Add int type to cover that php type.

---
+++ src/php/partial/fieldtype/int.php
@@ -0,0 +1,1 @@
+<input class="field value" type="number" step="1" name="<?= $field->name ?>" value="<?= $value ?>" autocomplete="off" style="width: 8em" <?= @$field->readonly ? 'disabled' : null ?>>
