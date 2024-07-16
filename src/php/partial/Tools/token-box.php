cc35bf03dc340aa1e8b5800f6dc0c59cfc038e59
Oran Fry <oran@emerg.co>
2024-07-16 21:25:58 +1200

    Tokens
    Tweak an attribute to the token field to try to get password managers to work with it.

--- src/php/partial/Tools/token-box.php
+++ src/php/partial/Tools/token-box.php
@@ -1,7 +1,7 @@
 <div class="inline-rel">
     <div class="inline-modal">
         <form id="tokenform" action="/change-token" method="post" class="only-super1200">
-            <input type="text" name="token" value="<?= AUTH_TOKEN ?>" autofill="one-time-code" placeholder="token" style="min-width: 10em; width: 100%; padding: 0.5em">
+            <input type="text" name="token" value="<?= AUTH_TOKEN ?>" autofill="current-password" placeholder="token" style="min-width: 10em; width: 100%; padding: 0.5em">
         </form>
     </div>
     <a href="#" class="inline-modal-trigger"><i class="icon icon--gray icon--ticket"></i></a>
