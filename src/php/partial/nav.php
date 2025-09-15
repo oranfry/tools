1a63b619458e070ecb39f5760833e8ff66d1eb17
Oran Fry <oran@emerg.co>
2025-09-15 15:54:28 +1200

    Plugins
    Avoid trying to iterate over null.

--- src/php/partial/nav.php
+++ src/php/partial/nav.php
@@ -2,7 +2,7 @@
 
 use \subsimple\Config;
 
-foreach (Config::get()->mounted ?? null as $plugin) {
+foreach (Config::get()->mounted ?? [] as $plugin) {
     (function (object $plugin) {
         @include APP_HOME . '/' . $plugin->path . '/src/php/partial/nav/tools-plugin.php';
     })($plugin);
